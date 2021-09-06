391333de1791839d65eda8cf46f25ceac04556db
mel campos <ivaniamcg@gmail.com>
2021-09-05 22:20:51 -0600

    cambio de color en Form2

--- proyecto/proyecto/Form2.Designer.cs
+++ proyecto/proyecto/Form2.Designer.cs
@@ -38,9 +38,10 @@ namespace proyecto
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(392, 145);
+            this.button1.Location = new System.Drawing.Point(560, 242);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(107, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Saludo";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,47 +50,53 @@ namespace proyecto
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(409, 13);
+            this.label1.Location = new System.Drawing.Point(584, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.Size = new System.Drawing.Size(103, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Bienvenido.";
             // 
             // txtsaludar
             // 
-            this.txtsaludar.Location = new System.Drawing.Point(353, 100);
+            this.txtsaludar.Location = new System.Drawing.Point(504, 167);
+            this.txtsaludar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtsaludar.Name = "txtsaludar";
-            this.txtsaludar.Size = new System.Drawing.Size(153, 23);
+            this.txtsaludar.Size = new System.Drawing.Size(217, 31);
             this.txtsaludar.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(289, 55);
+            this.label2.Location = new System.Drawing.Point(413, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(323, 15);
+            this.label2.Size = new System.Drawing.Size(487, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Este formulario te dara la Bienvenida ingresando tu nombre.";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(274, 100);
+            this.label3.Location = new System.Drawing.Point(391, 167);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.Size = new System.Drawing.Size(82, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Nombre.";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1143, 750);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtsaludar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
