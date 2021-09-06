b9b78db7d3693e7cd93127b83a2a0bd1aa90c5ad
GEOVANNI ALEXANDER GUERRERO MUNGUIA <USIS015221@ugb.edu.sv>
2021-09-05 21:55:45 -0600

    Corrección de texto en menus desplegables

--- proyecto/proyecto/Form1.Designer.cs
+++ proyecto/proyecto/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace proyecto
             this.saludoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.caluladoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lenguajeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conversorDePesoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.acercadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaParteIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,6 +45,7 @@ namespace proyecto
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.saludoToolStripMenuItem,
             this.caluladoraToolStripMenuItem,
@@ -52,36 +54,45 @@ namespace proyecto
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1289, 35);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // saludoToolStripMenuItem
             // 
             this.saludoToolStripMenuItem.Name = "saludoToolStripMenuItem";
-            this.saludoToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
-            this.saludoToolStripMenuItem.Text = "saludo";
+            this.saludoToolStripMenuItem.Size = new System.Drawing.Size(83, 29);
+            this.saludoToolStripMenuItem.Text = "Saludo";
             this.saludoToolStripMenuItem.Click += new System.EventHandler(this.saludoToolStripMenuItem_Click);
             // 
             // caluladoraToolStripMenuItem
             // 
             this.caluladoraToolStripMenuItem.Name = "caluladoraToolStripMenuItem";
-            this.caluladoraToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.caluladoraToolStripMenuItem.Size = new System.Drawing.Size(112, 29);
             this.caluladoraToolStripMenuItem.Text = "Caluladora";
             this.caluladoraToolStripMenuItem.Click += new System.EventHandler(this.caluladoraToolStripMenuItem_Click);
             // 
             // lenguajeToolStripMenuItem
             // 
+            this.lenguajeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.conversorDePesoToolStripMenuItem});
             this.lenguajeToolStripMenuItem.Name = "lenguajeToolStripMenuItem";
-            this.lenguajeToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
-            this.lenguajeToolStripMenuItem.Text = "Coversor de peso";
+            this.lenguajeToolStripMenuItem.Size = new System.Drawing.Size(178, 29);
+            this.lenguajeToolStripMenuItem.Text = "Conversor de peso";
             this.lenguajeToolStripMenuItem.Click += new System.EventHandler(this.lenguajeToolStripMenuItem_Click);
+            // 
+            // conversorDePesoToolStripMenuItem
+            // 
+            this.conversorDePesoToolStripMenuItem.Name = "conversorDePesoToolStripMenuItem";
+            this.conversorDePesoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.conversorDePesoToolStripMenuItem.Text = "Conversor de peso";
             // 
             // acercadeToolStripMenuItem
             // 
             this.acercadeToolStripMenuItem.Name = "acercadeToolStripMenuItem";
-            this.acercadeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
-            this.acercadeToolStripMenuItem.Text = "Acercade.";
+            this.acercadeToolStripMenuItem.Size = new System.Drawing.Size(105, 29);
+            this.acercadeToolStripMenuItem.Text = "Acerca de";
             this.acercadeToolStripMenuItem.Click += new System.EventHandler(this.acercadeToolStripMenuItem_Click);
             // 
             // ayudaToolStripMenuItem
@@ -91,41 +102,42 @@ namespace proyecto
             this.ayudaParteLlToolStripMenuItem,
             this.ayudaParteLllToolStripMenuItem});
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
-            this.ayudaToolStripMenuItem.Text = "Ayuda.";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(79, 29);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
             this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
             // 
             // ayudaParteIToolStripMenuItem
             // 
             this.ayudaParteIToolStripMenuItem.Name = "ayudaParteIToolStripMenuItem";
-            this.ayudaParteIToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ayudaParteIToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.ayudaParteIToolStripMenuItem.Text = "Ayuda parte I";
             this.ayudaParteIToolStripMenuItem.Click += new System.EventHandler(this.ayudaParteIToolStripMenuItem_Click);
             // 
             // ayudaParteLlToolStripMenuItem
             // 
             this.ayudaParteLlToolStripMenuItem.Name = "ayudaParteLlToolStripMenuItem";
-            this.ayudaParteLlToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ayudaParteLlToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.ayudaParteLlToolStripMenuItem.Text = "Ayuda Parte ll";
             this.ayudaParteLlToolStripMenuItem.Click += new System.EventHandler(this.ayudaParteLlToolStripMenuItem_Click);
             // 
             // ayudaParteLllToolStripMenuItem
             // 
             this.ayudaParteLllToolStripMenuItem.Name = "ayudaParteLllToolStripMenuItem";
-            this.ayudaParteLllToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ayudaParteLllToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.ayudaParteLllToolStripMenuItem.Text = "Ayuda Parte lll";
             this.ayudaParteLllToolStripMenuItem.Click += new System.EventHandler(this.ayudaParteLllToolStripMenuItem_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(902, 450);
+            this.ClientSize = new System.Drawing.Size(1289, 750);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -146,6 +158,7 @@ namespace proyecto
         private System.Windows.Forms.ToolStripMenuItem ayudaParteIToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaParteLlToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaParteLllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conversorDePesoToolStripMenuItem;
     }
 }
 
